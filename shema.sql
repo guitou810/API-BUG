3439978f4e0967e19b485b8a837ff1c3f114c846
Alexis Romero <romero.alexis@free.fr>
2020-03-04 18:53:48 +0100

    update schema

--- shema.sql
+++ shema.sql
@@ -2,8 +2,6 @@
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `bug`;
 CREATE TABLE `bug` (
@@ -11,9 +9,11 @@ CREATE TABLE `bug` (
   `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` text COLLATE utf8_unicode_ci DEFAULT NULL,
   `createdAt` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
-  `closed` tinyint(1) unsigned zerofill NOT NULL,
+  `closed` datetime DEFAULT NULL,
+  `domain` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ip` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
--- 2019-10-15 15:27:31
+-- 2020-03-04 17:52:27
